e50926a56299125ded242b1329cc148174d37931
Fabien <fabienswift@gmail.com>
2022-10-04 11:18:57 -0400

    Add food controller and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
 
+  has_many :foods, foreign_key: 'user_id', dependent: :destroy
+
   validates :name, presence: true
+
+  
 end
