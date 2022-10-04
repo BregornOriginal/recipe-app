08387685b9e26f68d0d7aaf9b1098b1410e59560
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-10-04 14:57:02 -0300

    Added new and create controllers for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,9 @@ class RecipesController < ApplicationController
 
   # GET /recipes or /recipes.json
   def index
-    @recipes = Recipe.all
+    @user = User.find(params[:user_id])
+    @recipes = Recipe.where(user_id: @user).order(updated_at: :asc).limit(2)
+    @current_user = current_user
   end
 
   # GET /recipes/1 or /recipes/1.json
@@ -11,19 +13,22 @@ class RecipesController < ApplicationController
 
   # GET /recipes/new
   def new
-    @recipe = Recipe.new
+    @user = current_user
+    recipe = Recipe.new
+    respond_to do |format|
+      format.html { render :new, locals: { recipe: } }
+    end
   end
-
-  # GET /recipes/1/edit
-  def edit; end
 
   # POST /recipes or /recipes.json
   def create
+    @user = current_user
+    @recipe = @user.recipe.create(recipe_params)
     @recipe = Recipe.new(recipe_params)
 
     respond_to do |format|
       if @recipe.save
-        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }
+        format.html { redirect_to user_recipes_url(@user.id), notice: 'Recipe was successfully created.' }
         format.json { render :show, status: :created, location: @recipe }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -44,13 +49,12 @@ class RecipesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_recipe
     @recipe = Recipe.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
   def recipe_params
-    params.require(:recipe).permit(:user_id, :name, :preperation_time, :cooking_time, :description, :public)
+    params.require(:recipe).permit(:name, :preperation_time, :cooking_time, :description, :public)
   end
 end
