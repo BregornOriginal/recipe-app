51572118ad07352cf4e99514f53290012f7eff6c
Fabien <fabienswift@gmail.com>
2022-10-05 16:55:18 -0400

    Add view for public recipe page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       resources :recipe_foods
     end
   end
+  resources :public_recipes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
