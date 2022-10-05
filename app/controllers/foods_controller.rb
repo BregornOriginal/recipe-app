32c071b7b36b724947391ee628fc2297d4561a7a
Fabien <fabienswift@gmail.com>
2022-10-04 22:28:02 -0400

    Add styles and fix views for food, also adjust food controller for delete button operation

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,9 +1,11 @@
 class FoodsController < ApplicationController
-  before_action :set_food, only: %i[show edit update destroy]
+  # before_action :set_food, only: %i[show edit update destroy]
+  before_action :authenticate_user!
 
   # GET /foods or /foods.json
   def index
-    @foods = Food.all
+    @users = User.find(params[:user_id])
+    @foods = @users.foods
   end
 
   # GET /foods/1 or /foods/1.json
@@ -11,19 +13,22 @@ class FoodsController < ApplicationController
 
   # GET /foods/new
   def new
+    @user = current_user
     @food = Food.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: @food } }
+    end
   end
 
-  # GET /foods/1/edit
-  def edit; end
 
   # POST /foods or /foods.json
   def create
-    @food = Food.new(food_params)
+    @user = current_user
+    @food = @user.foods.create(food_params)
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to user_foods_path, notice: 'Food was successfully created.' }
+        format.html { redirect_to user_foods_path(current_user), notice: 'Food was successfully created.' }
         format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -33,24 +38,16 @@ class FoodsController < ApplicationController
   end
 
   # PATCH/PUT /foods/1 or /foods/1.json
-  def update
-    respond_to do |format|
-      if @food.update(food_params)
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully updated.' }
-        format.json { render :show, status: :ok, location: @food }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+
 
   # DELETE /foods/1 or /foods/1.json
   def destroy
+    @user = current_user
+    @food = Food.find(params[:id])
     @food.destroy
 
     respond_to do |format|
-      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
+      format.html { redirect_to user_foods_url(@user.id), notice: 'Food was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -58,12 +55,9 @@ class FoodsController < ApplicationController
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_food
-    @food = Food.find(params[:id])
-  end
 
   # Only allow a list of trusted parameters through.
   def food_params
-    params.require(:food).permit(:user_id, :name, :measurement_unit, :price, :quantity)
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
 end
