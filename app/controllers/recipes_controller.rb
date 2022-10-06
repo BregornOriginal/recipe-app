b07bf4981bf13a61dd549d6f23bcf8de29df6ffc
Fabien <fabienswift@gmail.com>
2022-10-05 20:58:57 -0400

    refactor recipe code

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,13 +2,10 @@ class RecipesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @user = User.find(params[:user_id])
     @recipes = Recipe.where(user_id: @user).order(updated_at: :asc).limit(2)
-    @current_user = current_user
   end
 
   def show
-    @user = current_user
     @recipe = Recipe.find(params[:id])
     @recipes = Recipe.where(user_id: @user).order(updated_at: :asc).limit(2)
     @recipes = Recipe.find_by(id: params[:recipe_id])
@@ -16,7 +13,6 @@ class RecipesController < ApplicationController
   end
 
   def new
-    @user = current_user
     recipe = Recipe.new
     respond_to do |format|
       format.html { render :new, locals: { recipe: } }
@@ -24,12 +20,12 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @user = current_user
-    @recipe = @user.recipes.create(recipe_params)
+    @recipe= Recipe.new(recipe_params)
+    @recipe.user= current_user
 
     respond_to do |format|
       if @recipe.save
-        format.html { redirect_to user_recipes_path(@user.id), notice: 'Recipe was successfully created.' }
+        format.html { redirect_to recipes_path(current_user), notice: 'Recipe was successfully created.' }
         format.json { render :show, status: :created, location: @recipe }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -44,7 +40,7 @@ class RecipesController < ApplicationController
     @recipe.destroy
 
     respond_to do |format|
-      format.html { redirect_to user_recipes_path(@user.id), notice: 'Recipe was successfully destroyed.' }
+      format.html { redirect_to recipes_path(@user.id), notice: 'Recipe was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
