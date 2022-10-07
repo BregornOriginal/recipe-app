596342db6f797e9cc56aed0f45cade9af2fe0bc4
Fabien <fabienswift@gmail.com>
2022-10-06 21:35:14 -0400

    Shopping list function

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+  @subject = User.create(
+    name: 'Fabien',
+    email: 'test@gmail.com',
+    password: '123456',
+    confirmation_token: 'rxzG-sGyFUFKM5tMqKLf'
+  )
+
+  visit new_user_session_path
+  fill_in 'user_email', with: @user.email
+  fill_in 'user_password', with: @user.password
+  click_button('Log in')
+
+  it 'should have name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+end
 end
